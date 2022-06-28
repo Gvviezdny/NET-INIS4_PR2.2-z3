969e9f3229cc370411aec977400de435ce722a55
Marcin <Marcin@DESKTOP-STSUK8D>
2022-06-28 18:05:04 +0200

    Dodaj pliki projektów.

---
+++ Album.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_INIS4_PR2._2_z3
+{
+    public class Album : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private readonly static Dictionary<string, string[]> relatedProperties = new Dictionary<string, string[]>()
+        {
+            ["Tytuł"] = new string[] { "Tytuł", "FormatListy" },
+            ["Wydawca"] = new string[] { "Wydawca", "FormatListy" },
+            ["Nośnik"] = new string[] { "Nośnik", "FormatListy" },
+            ["Długość"] = new string[] { "Długość", "FormatListy" },
+            ["Reżyser_autor"] = new string[] { "Reżyser_autor", "FormatListy" }
+        };
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            if(relatedProperties.ContainsKey(propertyName))
+                foreach(string relatedProperty in relatedProperties[propertyName])
+                    OnPropertyChanged(relatedProperty);
+            //śledzenie zapobiegające stack overflow?
+        }
+
+        static uint następneID = 0;
+        /*uint wiek = 0;*/
+        string
+            Tytuł,
+            Wydawca,
+            Nośnik,
+            Długość
+            ;
+        string
+            Reżyser_autor = null
+            ;
+ 
+
+        public string _Tytuł {
+            get => Tytuł;
+            set
+            {
+                Tytuł = value;
+                OnPropertyChanged();
+            }
+        }
+        public string _Wydawca {
+            get => Wydawca;
+            set
+            {
+                Wydawca = value;
+                OnPropertyChanged();
+            }
+        }
+        public string _Nosnik
+        {
+            get => Nośnik;
+            set
+            {
+                Nośnik = value;
+                OnPropertyChanged();
+            }
+        }
+        public string _Długość {
+            get => Długość;
+            set
+            {
+                Długość = value;
+                OnPropertyChanged();
+            }
+        }
+        public string _Reżyser_autor
+        {
+            get => Reżyser_autor;
+            set
+            {
+                Reżyser_autor = value;
+                OnPropertyChanged();
+            }
+        }
+        public string FormatListy { get => $"{Tytuł},{Wydawca},{Nośnik},{Długość}, {Reżyser_autor}"; } 
+   
+        public uint ID { get; } = następneID++;
+        public string FormatID { get => "ID: " + ID; }
+    }
+}
---
+++ Model.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_INIS4_PR2._2_z4
+{
+    class Model
+    {
+
+        /*Prawdopodobnie brakuje jawnego pola z listą, właściwości jawnie zaimplementowanej i notyfikacji o zmianie*/
+        public ObservableCollection<Album> Lista { get; set; } = new ObservableCollection<Album>(new Album[] {
+            new Album() {
+                _Tytuł = "Majtrix",
+                _Wydawca = "Wiśniewski",
+                _Nosnik = "CD",
+                _Długość = "128minut",
+                _Reżyser_autor = "Iluminaci"
+            },
+            new Album() {
+                  _Tytuł = "GimmeOneMoreTime",
+                _Wydawca = "JorgeBush",
+                _Nosnik = "DVD",
+                _Długość = "60minut",
+                _Reżyser_autor = "BritnejSpirs"
+            },
+            new Album() {
+                _Tytuł = "LordOfTheRins",
+                _Wydawca = "WarnerBrothers",
+                _Nosnik = "CD",
+                _Długość = "228minut",
+                _Reżyser_autor = "TomHanks"
+            },
+
+        });
+
+        internal void OtwórzSzczegóły(Album wybrany)
+        {
+            Szczegóły szczegóły = new Szczegóły(wybrany);
+            szczegóły.Show();
+        }
+        internal void DodajNowy()
+        {
+            Album nowa = new Album();
+            Lista.Add(nowa);
+            Szczegóły szczegóły = new Szczegóły(nowa);
+            szczegóły.Show();
+            /*aktualizowanie widoku samej listy*/
+        }
+    }
+}
